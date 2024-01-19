99f60a4e89d5a17b6702ba5fd18b30321c226301
Doug Rabson <dfr@rabson.org>
2024-01-19 15:22:18 +0000

    bcm2835_vcbus: add Rpi5 support

--- sys/arm/broadcom/bcm2835/bcm2835_vcbus.c
+++ sys/arm/broadcom/bcm2835/bcm2835_vcbus.c
@@ -174,6 +174,11 @@ static struct bcm283x_memory_soc_cfg {
 		.soc_compat = "brcm,bcm2838",
 		.busdma_lowaddr = BCM2838_PERIPH_MAXADDR,
 	},
+	{
+		.memmap = bcm2838_memmap,
+		.soc_compat = "brcm,bcm2712",
+		.busdma_lowaddr = BCM2838_PERIPH_MAXADDR,
+	},
 };
 
 static struct bcm283x_memory_soc_cfg *booted_soc_memcfg;
